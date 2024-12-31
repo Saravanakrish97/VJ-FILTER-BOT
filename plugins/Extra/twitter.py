c980c1f9598a251e846c8e669db317b4119f0918
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-12-31 15:29:53 +0530

    Update twitter.py

--- plugins/Extra/twitter.py
+++ plugins/Extra/twitter.py
@@ -17,13 +17,13 @@ async def twitter_handler(Client, message):
          link = link.replace("twitter.com","fxtwitter.com")
       m=await message.reply_sticker("CAACAgIAAxkBATWhF2Qz1Y-FKIKqlw88oYgN8N82FtC8AAJnAAPb234AAT3fFO9hR5GfHgQ")
       try:
-          dump_file = await message.reply_video(link,caption="Thank you for using - @TamilMovies5K")
+          dump_file = await message.reply_video(link,caption="Thank you for using - @TamilMovies6K")
       except Exception as e:
           print(e)
           try:
              snd_message=await message.reply(link)
              await asyncio.sleep(1)
-             dump_file = await message.reply_video(link,caption="Thank you for using - @TamilMovies5K")
+             dump_file = await message.reply_video(link,caption="Thank you for using - @TamilMovies6K")
              await snd_message.delete()
           except Exception as e:
               print(e)
@@ -35,13 +35,13 @@ async def twitter_handler(Client, message):
                   meta_tag = soup.find("meta", attrs={"property": "og:image"})
               content_value  = meta_tag['content']
               try:
-                  dump_file = await message.reply_video(content_value,caption="Thank you for using - @TamilMovies5K")
+                  dump_file = await message.reply_video(content_value,caption="Thank you for using - @TamilMovies6K")
               except Exception as e:
                   print(e)
                   try:
                      snd_msg=await message.reply(content_value)
                      await asyncio.sleep(1)
-                     await message.reply_video(content_value,caption="Thank you for using - @TamilMovies5K")
+                     await message.reply_video(content_value,caption="Thank you for using - @TamilMovies6K")
                      await snd_msg.delete()
                   except Exception as e:
                       print(e)
@@ -56,5 +56,5 @@ async def twitter_handler(Client, message):
           if "dump_file" in locals():
              await dump_file.copy(DUMP_GROUP)
        await m.delete()
-       await message.reply("Check out @TamilMovies5k (music)  @TamilBots(Channel) \n Please Support Us By /donate To Maintain This Project")               
+       await message.reply("Check out @TamilMovies6K (music)  @TamilBots(Channel) \n Please Support Us By /donate To Maintain This Project")               
                   
