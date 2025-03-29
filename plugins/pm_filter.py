a5db549303e3542dd4ce84b576262e2ce7a3db18
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2025-03-29 13:15:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1343,60 +1343,42 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alerts[int(i)]
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
-        
+            
     if query.data.startswith("file"):
-        clicked = query.from_user.id
-        try:
-            typed = query.message.reply_to_message.from_user.id
-        except:
-            typed = query.from_user.id
-        ident, file_id = query.data.split("#")
-        files_ = await get_file_details(file_id)
-        if not files_:
-            return await query.answer('Nᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪsᴛ.')
-        files = files_[0]
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = files.caption
-        settings = await get_settings(query.message.chat.id)
-        if CUSTOM_FILE_CAPTION:
+            clicked = query.from_user.id
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-            f_caption = f_caption
-        if f_caption is None:
-            f_caption = f"{files.file_name}"
+                typed = query.message.reply_to_message.from_user.id
+            except:
+                typed = query.from_user.id
+            ident, file_id = query.data.split("#")
+            files_ = await get_file_details(file_id)
+            if not files_:
+                return await query.answer('Nᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪsᴛ.')
+            files = files_[0]
+            title = files.file_name
+            size = get_size(files.file_size)
+            f_caption = files.caption
+            settings = await get_settings(query.message.chat.id)
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
+                                                            file_size='' if size is None else size,
+                                                            file_caption='' if f_caption is None else f_caption)
+                except Exception as e:
+                    logger.exception(e)
+                f_caption = f_caption
+            if f_caption is None:
+                f_caption = f"{files.file_name}"
 
-        try:
-            if settings['is_shortlink'] and not await db.has_premium_access(query.from_user.id):
-                if SHORTLINK_MODE == True:
-                    if clicked == typed:
-                        temp.SHORT[clicked] = query.message.chat.id
-                        await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start=short_{file_id}")
-                        return
-                    else:
-                        await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !\n\n⚠️ வணக்கம்,\n இது உங்கள் கோரிக்கை அல்ல, உங்கள் கோரிக்கையை கோருங்கள்", show_alert=True)
-                else:
-                    await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}")
-#            elif settings['is_shortlink'] and await db.has_premium_access(query.from_user.id):
-#                if clicked == typed:
-#                    await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}")
-#                    return
-#                else:
-#                    await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !\n\n⚠️ வணக்கம்,\n இது உங்கள் கோரிக்கை அல்ல, உங்கள் கோரிக்கையை கோருங்கள்", show_alert=True)
-                    
-            else:
+            try:
                 if clicked == typed:
                     file_send = await client.send_cached_media(
                         chat_id=FILE_FORWARD,
                         file_id=file_id,
                         caption=script.CHANNEL_CAP.format(query.from_user.mention, title, query.message.chat.title),
-                  #      protect_content=True if ident == "filep" else False,
+                    #    protect_content=True if ident == "filep" else False,
                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📢 Channel", url=CHNL_LNK),  # we download Link
-                                                            InlineKeyboardButton('📽️ Group', url=GRP_LNK)]])  # web stream Link
+                                                                             InlineKeyboardButton('📽️ Group', url=GRP_LNK)]])  # web stream Link
                     )
                     Joel_tgx = await query.message.reply_text(
                         text=script.FILE_MSG.format(query.from_user.mention, title, size),
@@ -1406,8 +1388,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                 [
                                     InlineKeyboardButton('📥 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽 𝖫𝗂𝗇𝗄 📥 ', url=file_send.link)
                                 ], [
-                                InlineKeyboardButton("⚠️ 𝖢𝖺𝗇'𝗍 𝖠𝖼𝖼𝖾𝗌𝗌 ❓ 𝖢𝗅𝗂𝖼𝗄 𝖧𝖾𝗋𝖾 ⚠️", url=f'https://t.me/Tamil5k')
-                            ]
+                                    InlineKeyboardButton("⚠️ 𝖢𝖺𝗇'𝗍 𝖠𝖼𝖼𝖾𝗌𝗌 ❓ 𝖢𝗅𝗂𝖼𝗄 𝖧𝖾𝗋𝖾 ⚠️", url=f'https://t.me/Tamil5k')
+                                ]
                             ]
                         )
                     )
@@ -1420,12 +1402,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !\n\n⚠️ வணக்கம்,\n இது உங்கள் கோரிக்கை அல்ல, உங்கள் கோரிக்கையை கோருங்கள்",
                         show_alert=True)
                 await query.answer('Cʜᴇᴄᴋ, I ʜᴀᴠᴇ sᴇɴᴛ ғɪʟᴇs', show_alert=True)
-        except UserIsBlocked:
-            await query.answer('𝐔𝐧𝐛𝐥𝐨𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐦𝐚𝐡𝐧 !', show_alert=True)
-        except PeerIdInvalid:
-            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-        except Exception as e:
-            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+            except UserIsBlocked:
+                await query.answer('𝐔𝐧𝐛𝐥𝐨𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭 𝐦𝐚𝐡𝐧 !', show_alert=True)
+            except PeerIdInvalid:
+                await query.answer(f"Could not forward file. Please check the bot's permissions in the {FILE_FORWARD} channel.", show_alert=True)
+            except Exception as e:
+                await query.answer(f"An error occurred: {e}", show_alert=True)
     
     elif query.data.startswith("sendfiles"):
         clicked_user_id = query.from_user.id
